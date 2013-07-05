281738070cc561c678a50512de339d59d4e74f3d
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-07-05 13:32:31 +0200

    use mlmake preprocessor

---
+++ OMakefile.ml
@@ -0,0 +1,35 @@
+install Program ".DEFAULT" [
+  (* Target *)
+  Name		"hm";
+
+  (* Sources *)
+  Modules [
+    "Hm";
+    "Hm_ast";
+    "Hm_dynparse";
+    "Hm_env";
+    "Hm_errors";
+    "Hm_infer";
+    "Hm_lexer";
+    "Hm_parser";
+    "Hm_token";
+    "Hm_tokens";
+    "Hm_type";
+  ];
+
+  (* Library dependencies *)
+  OCamlRequires [
+    "batteries";
+    "merr";
+    "sexplib.syntax";
+  ];
+
+  (* Camlp4 *)
+  Flags [
+    "a_ast.ml",		"-syntax camlp4o";
+    "e_ast.ml",		"-syntax camlp4o";
+    "makeErr.ml",	"-syntax camlp4o";
+  ];
+
+  Var ("RUNMERR", "hm.native -merr");
+]
